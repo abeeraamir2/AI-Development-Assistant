82c2ce7c07b05bdea750a717470074ef0f50f0d7
Abeera Amir <161155798+abeeraamir2@users.noreply.github.com>
2026-08-22 21:32:52 +0500

    Pydantic model of user updated

--- backend/models/user_models.py
+++ backend/models/user_models.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
 
 class UserRegister(BaseModel):
+    name: Optional[str] = None
     email: str
     password: str
     role: str
@@ -8,3 +11,19 @@ class UserRegister(BaseModel):
 class UserLogin(BaseModel):
     email: str
     password: str
+
+
+class UserCreate(BaseModel):
+    name: Optional[str] = None
+    email: str
+    password: Optional[str] = "TempPass123!"
+    role: str
+    status: Optional[str] = "Active"
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    role: Optional[str] = None
+    status: Optional[str] = None
+    password: Optional[str] = None
